b0226a5f37cb95af13d7c86d0979d319e3d90cce
rksharma007 <rksbot3837@gmail.com>
2022-12-13 13:22:43 +0530

    resume-complete

--- client/src/components/resume/Resume.js
+++ client/src/components/resume/Resume.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import pdf from "../../assets/RESUME.pdf";
 import StarBg from '../layout/StarBg';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+function removeTextLayerOffset() {
+    const textLayers = document.querySelectorAll(".react-pdf__Page__textContent");
+      textLayers.forEach(layer => {
+        const { style } = layer;
+        style.top = "0";
+        style.left = "0";
+        style.transform = "";
+    });
+  }
+  
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://raw.githubusercontent.com/rksharma007/rksharma007/main/RESUME%20Ravi_Kant_Sharma.pdf";
 
 const Resume = () => {
   const [width, setWidth] = useState(1200);
@@ -22,35 +30,16 @@ const Resume = () => {
     <div>
       <Container fluid className="resume-section">
         <StarBg />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
+        <Row style={{ justifyContent: "center", position: "relative" }}  className="resume-section-download">
+          <a href={pdf} target="_blank" rel="noreferrer" style={{maxWidth:"200px"}}>
+            <i style={{color: "white"}} className="fa fa-download" >&nbsp;Download CV</i>
+          </a>
         </Row>
-
         <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={1} scale={width > 786 ? 1.5 : 0.5} onLoadSuccess={removeTextLayerOffset} />
           </Document>
         </Row>
-
-        {/* <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row> */}
       </Container>
     </div>
   );
