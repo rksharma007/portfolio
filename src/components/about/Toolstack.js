3a535a0d1a074a8c9abd78e89cc4e2df6b0b2c9a
R K Sharma <77050199+rksharma007@users.noreply.github.com>
2025-05-15 13:15:57 +0530

    ...

--- src/components/about/Toolstack.js
+++ src/components/about/Toolstack.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import {  SiArduino, SiRaspberrypi, SiKubernetes, SiPycharm, SiDbeaver, SiDocker, SiIntellijidea, SiAdobeillustrator, SiAdobexd, SiGithub, SiHeroku, SiLinux, SiNetlify, SiPostman, SiVercel, SiVisualstudiocode } from "react-icons/si";
+import { SiArduino, SiRaspberrypi, SiKubernetes, SiPycharm, SiDbeaver, SiDocker, SiIntellijidea, SiAdobeillustrator, SiGithub, SiHeroku, SiLinux, SiNetlify, SiPostman, SiVercel, SiVisualstudiocode } from "react-icons/si";
 import { DiOpenshift } from "react-icons/di";
 
 const Toolstack = () => {
 return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
         <Col xs={1} md={2} className="tech-icons" title="GitHub"><SiGithub /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="Linux"><SiLinux /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="VS Code"><SiVisualstudiocode /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="IntelliJ IDEA"><SiIntellijidea /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="PyCharm"><SiPycharm /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="Docker"><SiDocker /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="Kubernetes"><SiKubernetes /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="OpenShift"><DiOpenshift /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="DBeaver"><SiDbeaver /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="Adobe Illustrator"><SiAdobeillustrator /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="Raspberry Pi"><SiRaspberrypi /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="Arduino"><SiArduino /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="Postman"><SiPostman /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="Heroku"><SiHeroku /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="Netlify"><SiNetlify /></Col>
-      <Col xs={1} md={2} className="tech-icons" title="Vercel"><SiVercel /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="Linux"><SiLinux /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="VS Code"><SiVisualstudiocode /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="IntelliJ IDEA"><SiIntellijidea /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="PyCharm"><SiPycharm /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="Docker"><SiDocker /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="Kubernetes"><SiKubernetes /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="OpenShift"><DiOpenshift /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="DBeaver"><SiDbeaver /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="Adobe Illustrator"><SiAdobeillustrator /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="Raspberry Pi"><SiRaspberrypi /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="Arduino"><SiArduino /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="Postman"><SiPostman /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="Heroku"><SiHeroku /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="Netlify"><SiNetlify /></Col>
+        <Col xs={1} md={2} className="tech-icons" title="Vercel"><SiVercel /></Col>
     </Row>
 )};
     
